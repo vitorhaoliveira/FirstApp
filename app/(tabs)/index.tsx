3347d5cbe3108e600884d55358f614ce0269030d
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2024-07-29 22:37:53 -0300

    Commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello World</ThemedText>
+        <ThemedText type="title">Hello World!</ThemedText>
         <HelloWave />
       </ThemedView>
     </ParallaxScrollView>
